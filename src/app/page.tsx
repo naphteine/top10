6451806fdc2718962727939c407160f32faf9479
Gökay Gültekin <58526116+naphteine@users.noreply.github.com>
2023-07-29 15:22:33 +0300

    Add website link

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 import Chat from "./chat";
-import { PiGithubLogoDuotone } from "react-icons/pi";
+import { PiGithubLogoDuotone, PiPillDuotone } from "react-icons/pi";
 
 export const runtime = "edge";
 
@@ -8,9 +8,16 @@ export default function Page() {
     <main className="min-h-screen mt-10 flex flex-col items-center">
       <h1 className="text-4xl font-bold">Top 10 Generator</h1>
       <em>Generate top 10 list on any topic, using OpenAI</em>
-      <a href="https://github.com/naphteine/top10">
-        <PiGithubLogoDuotone className="mb-5 hover:text-yellow-300" size={32} />
-      </a>
+
+      <div className="flex mb-5">
+        <a href="https://github.com/naphteine/top10">
+          <PiGithubLogoDuotone className="hover:text-yellow-300" size={32} />
+        </a>
+        <a href="https://gokaygultekin.dev">
+          <PiPillDuotone className="hover:text-yellow-300" size={32} />
+        </a>
+      </div>
+
       <Chat />
     </main>
   );
