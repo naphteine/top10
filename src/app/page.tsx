d6af6dee5433d9cd53a8b29f9bfda6d681873e19
Gökay Gültekin <58526116+naphteine@users.noreply.github.com>
2023-07-29 15:10:36 +0300

    Add header and introduction text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,8 @@ export const runtime = "edge";
 export default function Page() {
   return (
     <main className="min-h-screen mt-10 flex flex-col items-center">
+      <h1 className="text-4xl font-bold">Top 10 Generator</h1>
+      <em className="mb-5">Generate top 10 list on any topic, using OpenAI</em>
       <Chat />
     </main>
   );
